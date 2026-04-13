4f9b4ed58dde04e03457e9f7df09a45a4a4dd958
PPPmare <riku14274@gmail.com>
2026-04-13 15:06:07 +0900

    Camera Commit

---
+++ Assets/saegusa/あああ.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class あああ : MonoBehaviour
+{
+
+    private float[] positions = { -4f, 0f, 4f };
+    private int currentLane = 1;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
