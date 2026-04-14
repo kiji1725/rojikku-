7f96fbb07dba7e831f5983e4b1419a3d19f9ab41
hatopo0811 <hatopo0811@gmail.com>
2026-04-14 15:20:15 +0900

    PlayerMome Commit

---
+++ Assets/takegoshi/Script/PlayerMove.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // ステージ上でプレイヤーを移動させるためのスピード
+    [SerializeField] float speed = 5.0f;
+
+    float moveX = 0.0f;
+    float moveZ = 0.0f;
+
+    Vector3 move;
+
+    void Start()
+    {
+        move = new Vector3(moveX, 0, moveZ).normalized;
+
+    }
+    private void FixedUpdate()
+    {
+        transform.position += move * speed * Time.deltaTime;
+    }
+    void Update()
+    {
+        moveX = 0.0f;
+        moveZ = 0.0f;
+
+        if (Input.GetKey(KeyCode.T)) moveZ += 1f;
+        if (Input.GetKey(KeyCode.G)) moveZ -= 1f;
+        if (Input.GetKey(KeyCode.F)) moveX -= 1f;
+        if (Input.GetKey(KeyCode.H)) moveX += 1f;
+        
+        move = new Vector3(moveX, 0, moveZ).normalized;
+    
+    }
+    
+}
