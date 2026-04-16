dc1f1bde6748f3b24f33f7a06756b43547b16d34
kiji1725 <kiji1725ito@gmail.com>
2026-04-16 11:29:18 +0900

    PickUp

---
+++ Assets/BigRookGames/Scripts/Weapons/PickUp.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    // --- ’e”ŠÇ— ---
+    public int currentAmmo = 0;
+    public int maxAmmo = 50;
+
+    // --- ’e‚ğ‘‚â‚· ---
+    public void AddAmmo(int amount)
+    {
+        currentAmmo += amount;
+
+        if (currentAmmo > maxAmmo)
+            currentAmmo = maxAmmo;
+
+        Debug.Log("Œ»İ‚Ì’e”: " + currentAmmo);
+    }
+
+    // --- ’e‚ğg‚¤ ---
+    public bool UseAmmo(int amount)
+    {
+        if (currentAmmo >= amount)
+        {
+            currentAmmo -= amount;
+            return true;
+        }
+
+        return false;
+    }
+
+    // --- E‚¤ˆ— ---
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ammo"))
+        {
+            AddAmmo(10);
+            Destroy(other.gameObject);
+        }
+    }
+}
