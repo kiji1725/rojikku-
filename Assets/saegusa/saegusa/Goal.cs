a573a5e6ce739ed0f1fb5f78eacea83cb42bae47
PPPmare <riku14274@gmail.com>
2026-04-16 17:42:26 +0900

    stage

--- Assets/saegusa/saegusa/Goal.cs
+++ Assets/saegusa/saegusa/Goal.cs
@@ -1,9 +1,21 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
+    
     private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            GOAL();
+
+
+        }
+    }
+    public void GOAL()
     {
         Debug.Log("ÉSÅ[Éã");
         SceneManager.LoadScene("Clear 1");
