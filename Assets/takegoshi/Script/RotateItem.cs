734588b54d0bca66f17c8020e84f059f0eada727
hatopo0811 <hatopo0811@gmail.com>
2026-04-16 10:29:41 +0900

    Item Rocket Commit

---
+++ Assets/takegoshi/Script/RotateItem.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class RotateItem : MonoBehaviour
+{
+
+    float startPos;
+    float speed = 2.0f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position.y;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // —‚¿‚Ä‚¢‚éƒAƒCƒeƒ€‚Ìã‰ºˆÚ“®‚Æ‰ñ“]
+        // ‰ºŒÀ0.5ãŒÀ1.0
+        float posY = startPos + Mathf.Sin(Time.time * speed) * 0.5f;
+
+        transform.position = new Vector3(transform.position.x, posY, transform.position.z);
+        transform.Rotate(0, 0, 90 * Time.deltaTime * 0.75f);
+
+    }
+}
