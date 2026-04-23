32a9e370c3979ebe6691fa8dcaec13294baa0ada
kiji1725 <kiji1725ito@gmail.com>
2026-04-23 11:56:48 +0900

    Scene Change

---
+++ Assets/Ito/Script/Normal.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Normal : MonoBehaviour
+{
+    public void OnClickNormal()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+}
