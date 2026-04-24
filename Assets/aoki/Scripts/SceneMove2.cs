037f364f2c0e82bd56c59c21a6a7b228b853367d
PPPmare <riku14274@gmail.com>
2026-04-24 12:15:55 +0900

    Fade In/Out

--- Assets/aoki/Scripts/SceneMove2.cs
+++ Assets/aoki/Scripts/SceneMove2.cs
@@ -1,10 +1,41 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneMove2 : MonoBehaviour
 {
-    public void GoSelect()
+    public UnityEngine.UI.Image fadePanel;
+    public float fadeDuration = 1f;
+    public string nextSceneName;
+
+    // É{É^ÉìÇ©ÇÁåƒÇ‘
+    public void StartFade()
     {
-        FadeManager.Instance.LoadSceneWithFade("DifficultySelection");
+        fadePanel.transform.SetAsLastSibling(); // ç≈ëOñ Ç…
+        StartCoroutine(FadeAndLoad());
+    }
+
+    IEnumerator FadeAndLoad()
+    {
+        yield return StartCoroutine(FadeOut());
+        SceneManager.LoadScene("DifficultySelection");
+    }
+
+    IEnumerator FadeOut()
+    {
+        float time = 0f;
+        Color color = fadePanel.color;
+
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            color.a = time / fadeDuration;
+            fadePanel.color = color;
+            yield return null;
+        }
+
+        color.a = 1f;
+        fadePanel.color = color;
     }
 }
