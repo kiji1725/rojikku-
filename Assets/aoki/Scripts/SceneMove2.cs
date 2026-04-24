42bc087aff420b711ef8f4f1834fdde32d9e466f
aoki0803 <nhs110315@gmail.com>
2026-04-24 11:13:41 +0900

    Scene

--- Assets/aoki/Scripts/SceneMove2.cs
+++ Assets/aoki/Scripts/SceneMove2.cs
@@ -5,6 +5,6 @@ public class SceneMove2 : MonoBehaviour
 {
     public void GoSelect()
     {
-        SceneManager.LoadScene("ModeSelect");
+        FadeManager.Instance.LoadSceneWithFade("ModeSelect");
     }
 }
