ed3e1efb9a79f8ab19af44588e7e7948458e434b
hatopo0811 <hatopo0811@gmail.com>
2026-05-07 11:31:43 +0900

    ray

--- Assets/BigRookGames/Scripts/Weapons/GunfireController.cs
+++ Assets/BigRookGames/Scripts/Weapons/GunfireController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 
 
+
 namespace BigRookGames.Weapons
 {
     public class GunfireController : MonoBehaviour
--- Assets/takegoshi/Script/RayLeft.cs
+++ Assets/takegoshi/Script/RayLeft.cs
@@ -1,32 +1,27 @@
 using UnityEngine;
 
-public class Raycast : MonoBehaviour
+public class RayLeft : MonoBehaviour
 {
-
     [SerializeField] private PlayerMove player;
 
     [SerializeField] private float rayDistance = 10.0f;
 
     public float rayPosY = 0.0f;
 
-    
-
-    Vector3 hitPosRight = Vector3.zero;
     Vector3 hitPosLeft = Vector3.zero;
 
-    
-
-    bool wallRunRight = false;
     bool wallRunLeft = false;
 
-    public   float frontWeight = 1.0f;
+    public float frontWeight = 1.0f;
 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-        //transform.Rotate(0.0f, rayRotate, 0.0f);
-
+        
     }
+
+    // Update is called once per frame
     void Update()
     {
 
@@ -38,10 +33,8 @@ public class Raycast : MonoBehaviour
 
         // RaycastHit には当たったオブジェクトの情報が入る
         RaycastHit hitLeft;
-        RaycastHit hitRight;
 
-        // 右前方向
-        Vector3 rightFront = (transform.forward + transform.right * frontWeight).normalized;
+        
         // 左前方向
         Vector3 leftFront = (transform.forward - transform.right * frontWeight).normalized;
 
@@ -56,40 +49,24 @@ public class Raycast : MonoBehaviour
 
             Debug.Log("左");
             Debug.Log("左当たった場所 : " + hitLeft.point);
-            
+
             hitPosLeft = hitLeft.point;
         }
-        // 右側のRay
-        if (Physics.Raycast(transform.position, rightFront, out hitRight, rayDistance))
-        {
-            //Debug.Log("ヒットまでの距離: " + hit.distance);
-            wallRunRight = true;
-            Debug.Log("右");
-            Debug.Log("右当たった場所 : " + hitRight.point);
-
-            hitPosRight = hitRight.point;
-        }
+ 
 
         // 左
         if (!Physics.Raycast(transform.position, leftFront, out hitLeft, rayDistance))
         {
-            
+
             wallRunLeft = false;
             Debug.Log("左当たってない");
-        
-            hitPosLeft = Vector3.zero;
-        
-        }
-
-        // 右
-        if (Physics.Raycast(transform.position, rightFront, out hitRight, rayDistance))
-        {
-            wallRunRight = false;
-            Debug.Log("右当たってない");
 
             hitPosLeft = Vector3.zero;
+
         }
-        
+
+       
+
 
 
 
@@ -97,16 +74,14 @@ public class Raycast : MonoBehaviour
 
         // 赤い線と青い線を描画
         Debug.DrawRay(ray.origin, leftFront * rayDistance, Color.red);
-        Debug.DrawRay(ray.origin, rightFront * rayDistance, Color.blue);
 
 
 
     }// update
 
     // Rayが当たった座標で分岐してこのフラグをtrueにすると壁走りができるようにする
-    public bool IsWallRunRight { get { return wallRunRight;  } }
-    public bool IsWallRunLeft { get { return wallRunLeft; } }
 
+    public bool IsWallRunLeft { get { return wallRunLeft; } }
 
 
 
