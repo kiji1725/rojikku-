a4767e95c881745ec81ccc006716a1cd4e84a908
kiji1725 <kiji1725ito@gmail.com>
2026-05-12 12:27:08 +0900

    Pose

--- Assets/Ito/Script/Pose.cs
+++ Assets/Ito/Script/Pose.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Pose : MonoBehaviour
 {
     private bool isPaused = false;
+    public Button pauseButton;
 
     void Update()
     {
@@ -24,11 +25,20 @@ public class Pose : MonoBehaviour
     {
         Time.timeScale = 0f;
         isPaused = true;
+        SetButtonColor(new Color(0.4f, 0.8f, 1f));
     }
 
     public void ResumeGame()
     {
         Time.timeScale = 1f;
         isPaused = false;
+        SetButtonColor(Color.white);
+    }
+
+    void SetButtonColor(Color color)
+    {
+        ColorBlock colors = pauseButton.colors;
+        colors.normalColor = color;
+        pauseButton.colors = colors;
     }
 }
